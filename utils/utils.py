aa24b6af41a262d3dd3a69e89f564fc205611c74
Michal Ziemski <mziemski@ethz.ch>
2023-05-08 11:13:34 +0200

    More Colab improvements

--- utils/utils.py
+++ utils/utils.py
@@ -12,9 +12,18 @@ def ensure_packages(requirements_file: str):
     with open(requirements_file, 'r') as f:
         packages = [line.strip() for line in f.readlines()]
 
+    to_install = ""
     for pkg in packages:
         try:
             get_distribution(pkg.split("==")[0])
         except (DistributionNotFound, RequirementParseError):
-            print(f"{pkg} not found. Installing...")
-            subprocess.check_call([sys.executable, "-m", "pip", "install", pkg])
+            print(f"{pkg} not found. Will be installed...")
+            to_install += f"{pkg} " if "jupyter" not in pkg else False
+
+    try:
+        print(f"Installing missing dependencies: {to_install}")
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", to_install]
+        )
+    except Exception as e:
+        print("Installation failed with error:", e)
